426e7e7eb8a4e2b4e246f149242bc055de4be47e
Tate <roxiee.aveley@gmail.com>
2014-02-28 23:08:38 -0600

    Instruccionesstring

--- src/projectovi/JFrameParabolico.java
+++ src/projectovi/JFrameParabolico.java
@@ -374,6 +374,7 @@ public class JFrameParabolico extends JFrame implements Runnable, KeyListener, M
                         //Pinta el Score y las vidas en la parte superior izquierda
                         g.drawString("Score:" + Integer.toString(score), 10, 70);
                         g.drawString("Vidas:" + Integer.toString(vidas), 10, 50);
+                        g.drawString("Instrucciones 'I'",10,90);
                     }else{
                             //Da un mensaje mientras se carga el dibujo	
                             g.drawString("No se cargo la imagen..",20,20);
